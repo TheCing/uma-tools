bf0213ad768ad6fdc9aefd062e676ee82eae363c
JP Tyndall <jptyndalljr@gmail.com>
2026-03-08 22:15:06 -0400

    Copy simulator.worker.js to root in build

--- build-all.sh
+++ build-all.sh
@@ -23,6 +23,7 @@ cd ../..
 echo "Copying v2 to root..."
 cp umalator-global/v2/dist/index.html ./index.html
 cp umalator-global/v2/dist/favicon.svg ./favicon.svg 2>/dev/null || true
+cp umalator-global/v2/dist/simulator.worker.js ./simulator.worker.js 2>/dev/null || true
 rm -rf assets
 cp -r umalator-global/v2/dist/assets ./assets
 
